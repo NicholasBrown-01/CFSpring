bf09e69ee82cd399829780b6df95f1baea311ea7
NicholasBrown-01 <nicholasbrown.us@gmail.com>
2023-07-24 20:18:59 -0500

    Lab 11 Complete

--- songr/src/main/java/com/cfjava/songr/controllers/albumController.java
+++ songr/src/main/java/com/cfjava/songr/controllers/albumController.java
@@ -1,17 +1,30 @@
 package com.cfjava.songr.controllers;
 
+import com.cfjava.songr.models.Album;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class albumController {
 
 @GetMapping("/albums")
     public String getAlbumsPage(Model m) {
+    //Hardcoded albums
+    Album woods = new Album("Woods", "DarkLight", 5, 3200, "images/WoodDoor.jpg");
+    Album rocker = new Album("Rocker", "LongHair", 8, 4200, "images/Rocker.jpg");
+    Album ripple = new Album("Ripple", "WatersCold", 10, 4800, "images/Ripple.jpg");
 
+    //Make list
+    List<Album> albums = Arrays.asList(woods, rocker, ripple);
 
+    //Adding the Album List to the Model
+    m.addAttribute("albums", albums);
 
     return "albums.html";
 }
--- songr/src/main/java/com/cfjava/songr/models/Album.java
+++ songr/src/main/java/com/cfjava/songr/models/Album.java
@@ -5,13 +5,53 @@ public class Album {
     String artist;
     int songCount;
     int length;
-        String imageURL;
+    String imageURL;
 
     public Album(String title, String artist, int songCount, int length, String imageURL) {
         this.title = title;
         this.artist = artist;
         this.songCount = songCount;
         this.length = length;
+        this.imageURL = imageURL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
         this.imageURL = imageURL;
     }
 }
