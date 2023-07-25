7375758a11dee387b91e676fd90394eac88d01bd
NicholasBrown-01 <nicholasbrown.us@gmail.com>
2023-07-25 12:59:15 -0500

    Added Dependencies and Changed Albums Array to Array List

--- songr/src/main/java/com/cfjava/songr/controllers/albumController.java
+++ songr/src/main/java/com/cfjava/songr/controllers/albumController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,7 +20,12 @@ public class albumController {
     Album ripple = new Album("Ripple", "WatersCold", 10, 4800, "images/Ripple.jpg");
 
     //Make list
-    List<Album> albums = Arrays.asList(woods, rocker, ripple);
+//    List<Album> albums = Arrays.asList(woods, rocker, ripple); // This would be an Array (non changeable in size)
+//    List<Album> albums = new ArrayList<>(Arrays.asList(woods, rocker, ripple)); // This would be a list, within an ArrayList. (changeable in size)
+    List<Album> albums = new ArrayList<>();
+    albums.add(woods);
+    albums.add(rocker);
+    albums.add(ripple);
 
     //Adding the Album List to the Model
     m.addAttribute("albums", albums);
